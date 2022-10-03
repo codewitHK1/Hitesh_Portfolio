acc1c029e5414277d9e66ba92e3372484568f778
unknown <kumarhk1002@gmail.com>
2022-10-03 10:53:10 +0530

    add more projects

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // import Sidebar from "../../img/sidebar.png";
 import Ecommerce from "../../img/ecommerce.png";
 import Todo from "../../img/TODO LIST.png";
+import Instabook from "../../img/Instabook.png";
 import weatherapp from "../../img/HKWEATHER.png";
 import food from "../../img/FOOD.jpg";
 import portfolio from "../../img/PORTFOLIO.png";
@@ -21,9 +22,19 @@ const Portfolio = () => {
       {/*Heading */}
       <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
       <span>Portfolio</span>
+      <div className="mern">
+        <h3>main Project -MERN SOCIAL MEDIA APP</h3>
+        <a
+          href="https://instabook001.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Instabook} alt="" />
+        </a>
+      </div>
       {/*slider*/}
       <Swiper
-        spaceBetween={30}
+        spaceBetween={27}
         slidesPerView={3}
         grabCursor={true}
         className="portfolio-slider"
